f9ad695360237dec0214cc5b2cae73cef85fa401
Mijk <dipalma10@hotmail.com>
2023-10-30 17:13:04 +0100

    Update Create VM and Auto enrollment 0.9.ps1

--- Create VM and Auto enrollment 0.9.ps1
+++ Create VM and Auto enrollment 0.9.ps1
@@ -35,7 +35,8 @@ function waitForPSDirect([string]$VMName, $cred){
     Write-Host "Create VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -F Y     New-VHD -Path "D:\ClusterStorage\Volume1\$VMName\$VMName.vhdx” -ParentPath "D:\ClusterStorage\Volume1\Hard Disk.vhdx" -Differencing | Out-Null
+    Write-Host " (Done)" -F Y     
+    New-VHD -Path "D:\ClusterStorage\Volume1\$VMName\$VMName.vhdx” -ParentPath "D:\ClusterStorage\Volume1\Hard Disk.vhdx" -Differencing | Out-Null
     Write-Host "Create vhdx Disk for VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
@@ -48,11 +49,13 @@ function waitForPSDirect([string]$VMName, $cred){
     Write-Host "Set 2 CPU nn $VMName : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -F Y     Add-VMHardDiskDrive -VMName $VMName -ControllerType IDE -ControllerNumber 0 -Path "D:\ClusterStorage\Volume1\$VMName\$VMName.vhdx”
+    Write-Host " (Done)" -F Y     
+    Add-VMHardDiskDrive -VMName $VMName -ControllerType IDE -ControllerNumber 0 -Path "D:\ClusterStorage\Volume1\$VMName\$VMName.vhdx”
     Write-Host "ADD VHDX disk to VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     Enable-VMIntegrationService –Name "Guest Service Interface" -VMName $VMName
+    Write-Host " (Done)" -f Y     
+    Enable-VMIntegrationService –Name "Guest Service Interface" -VMName $VMName
     Write-Host "Enable 'Guest Service Interface' on VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
@@ -61,27 +64,33 @@ function waitForPSDirect([string]$VMName, $cred){
     Write-Host "Now when configuration is finished on VM '$VMName' time to boot it up : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     Write-Host "Waiting for Powershell Direct is ready on VM '$VMName' : ” -ForegroundColor Y -NoNewline
+    Write-Host " (Done)" -f Y     
+    Write-Host "Waiting for Powershell Direct is ready on VM '$VMName' : ” -ForegroundColor Y -NoNewline
     waitForPSDirect -VMName $VMName -cred $cred  | Out-Null
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {Install-PackageProvider -Name NuGet -Confirm:$false -Force:$true} | Out-Null
+    Write-Host " (Done)" -f Y     
+    Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {Install-PackageProvider -Name NuGet -Confirm:$false -Force:$true} | Out-Null
     Write-Host "Running Install-PackageProvider -Name NuGet on VM '$VMName' time to boot it up : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {Install-Script Get-WindowsAutopilotInfo -Confirm:$false -Force:$true} | Out-Null
+    Write-Host " (Done)" -f Y     
+    Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {Install-Script Get-WindowsAutopilotInfo -Confirm:$false -Force:$true} | Out-Null
     Write-Host "Running Install-Script Get-WindowsAutopilotInfo on VM '$VMName' : ” -F y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {Powershell Set-ExecutionPolicy bypass -Force} | Out-Null
+    Write-Host " (Done)" -f Y     
+    Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {Powershell Set-ExecutionPolicy bypass -Force} | Out-Null
     Write-Host "Running Powershell Set-ExecutionPolicy bypass -Force on VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {set-timezone "W. Europe Standard Time"} | Out-Null
+    Write-Host " (Done)" -f Y     
+    Invoke-Command -VMName $VMName -Credential $cred -ScriptBlock {set-timezone "W. Europe Standard Time"} | Out-Null
     Write-Host "Running 'Set timezone(W. Europe Standard Time)' on VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     Get-VMIntegrationService -VMName $VMName -Name "Time Synchronization" | Enable-VMIntegrationService
+    Write-Host " (Done)" -f Y     
+    Get-VMIntegrationService -VMName $VMName -Name "Time Synchronization" | Enable-VMIntegrationService
     Write-Host "Running 'Time Synchronization' on VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
@@ -92,14 +101,16 @@ function waitForPSDirect([string]$VMName, $cred){
     Write-Host "Running 'Get-windowsautopilotinfo' on VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y     $Ser = Get-WmiObject -ComputerName $HypervHost -Namespace root\virtualization\v2 -class Msvm_VirtualSystemSettingData | ? {$_.elementName -eq $VMName} | Select -ExpandProperty BIOSSerialNumber
+    Write-Host " (Done)" -f Y     
+    $Ser = Get-WmiObject -ComputerName $HypervHost -Namespace root\virtualization\v2 -class Msvm_VirtualSystemSettingData | ? {$_.elementName -eq $VMName} | Select -ExpandProperty BIOSSerialNumber
     Write-Host "Running 'Get Serial number ($ser)' on VM '$VMName' : ” -F Y -NoNewline
     Start-Sleep -Seconds 1
     Write-Host @greenCheck
-    Write-Host " (Done)" -f Y         $TenantId = "e91cdf8b-9248-4fc8-b2121057-09216aa53747" 
+    Write-Host " (Done)" -f Y         
+    	$TenantId = "Blablabla" 
         $AppId = "Blablabla"
         $AppSecret = "Blablabla"         
-	  $clientId = "Blablabla" #Provide the Client ID
+	$clientId = "Blablabla" #Provide the Client ID
         $clientSecret = "Blablabla" # Provide the ClientSecret
         $ourTenantId = "Blablabla" #Specify the TenatID         
         $Resource = "deviceManagement/managedDevices"
